464c44957c5d8af2eb61cb47436f33fbd908f412
jdgutierrezp <jdgutierrezp@unal.edu.co>
2026-05-27 11:48:05 -0500

    ejr 4 + observaciones

--- stm32-project/Src/main.c
+++ stm32-project/Src/main.c
@@ -32,9 +32,13 @@ uint8_t a = 0;
 uint16_t b = 0;
 uint32_t c = 0;
 uint8_t d = 0;
+uint8_t e= 0;
+int8_t f=0;
+int8_t g = 0;
 
 
-main()
+
+int main(void)
 
 {
 		my_variable = 42;
@@ -44,7 +48,12 @@ main()
 		a =250;//al parecer este valor da -1 es por el int, lo que dice es que hay -1 antes de que secomplete el rango//
 		b=255;
 		c=255;
-		d=256;
+		d=255;
+		e = 200;
+		f = 200;
+		g = -1;// al parecer la condicion del signo es la ubicacion relativa del los datos//
+
+
 
 
 
