84b30297f329766246c53b8f3560dd80988203d5
jdgutierrezp <jdgutierrezp@unal.edu.co>
2026-06-10 17:02:01 -0500

    ejrc 2.4 +obs

--- stm32-project/Src/main.c
+++ stm32-project/Src/main.c
@@ -43,12 +43,13 @@
 
 
 /*varible definition*/
-uint8_t a=0;
+uint8_t mask =0;
+
 uint8_t result1 = 0;
-uint8_t b=0;
+
 uint8_t result2 = 0;
-uint8_t value = 0b11111111;
-uint8_t result =0;
+uint8_t value = 0;
+
 
 
 
@@ -60,15 +61,11 @@ uint8_t result =0;
 
 /* funcion main ......*/
 int main(void){
-	a=0b00001111;
-	result1 = ~a;
-	b=0b10100101;
-	result2 = ~b;
 
-	value &= ~(result1); /*el &= es el AND y el "~(valor a cambiar)", esto me permite crear una
-	mascara para poder cambiar los bits especificos*/
-
-
+	value = 0b10110011;
+	mask = 0b00001111;
+	result1 = value ^mask;
+	result2 = result1 ^mask; /* XOR aplicado al amsma mascara siempre regresa la valor original*/
 
 
 
